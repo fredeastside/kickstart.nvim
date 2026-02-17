fd318692e9fda78df18da03991802a06c93eaa1a
Sergei Kaznakhovskii <sergei.kaznakhovskii@prizepicks.com>
2026-02-17 11:12:23 -0700

    remove fterm

--- lua/plugins/fterm.lua
+++ lua/plugins/fterm.lua
@@ -1,22 +1,23 @@
-return {
-  {
-    'numToStr/FTerm.nvim',
-    config = function()
-      local map = vim.api.nvim_set_keymap
-      local opts = { noremap = true, silent = true }
-      require('FTerm').setup {
-        blend = 5,
-        dimensions = {
-          height = 0.90,
-          width = 0.90,
-          x = 0.5,
-          y = 0.5,
-        },
-      }
-
-      -- Fterm
-      map('n', '<leader>tt', ":lua require('FTerm').toggle()<CR>", { noremap = true })
-      map('t', '<leader>tt', '<C-\\><C-n>:lua require("FTerm").toggle()<CR>', { noremap = true })
-    end,
-  },
-}
+return {}
+-- return {
+--   {
+--     'numToStr/FTerm.nvim',
+--     config = function()
+--       local map = vim.api.nvim_set_keymap
+--       local opts = { noremap = true, silent = true }
+--       require('FTerm').setup {
+--         blend = 5,
+--         dimensions = {
+--           height = 0.90,
+--           width = 0.90,
+--           x = 0.5,
+--           y = 0.5,
+--         },
+--       }
+--
+--       -- Fterm
+--       map('n', '<leader>tt', ":lua require('FTerm').toggle()<CR>", { noremap = true })
+--       map('t', '<leader>tt', '<C-\\><C-n>:lua require("FTerm").toggle()<CR>', { noremap = true })
+--     end,
+--   },
+-- }
