75b5572adf68ac0a4a07560adb18b524900f3235
Sergei Kaznakhovskii <sergei.kaznakhovskii@prizepicks.com>
2026-05-26 11:02:14 -0600

    disable lsp codelens

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -11,6 +11,7 @@ return {
         lsp_inlay_hints = {
           enable = false,
         },
+        lsp_codelens = false,
       }
       local format_sync_grp = vim.api.nvim_create_augroup('GoImport', {})
       vim.api.nvim_create_autocmd('BufWritePre', {
